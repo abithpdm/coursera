795308a89b6476005b4108c0bc799622ab5b429d
Abith <abithpdm@gmail.com>
2020-06-21 23:09:20 +0530

    added comments

--- best.R
+++ best.R
@@ -1,9 +1,10 @@
 
 best<-function(state,outcome)
 {
+  #read the csv file
   hospitaldata<-read.csv("outcome-of-care-measures.csv",colClasses = "character")
+  #set index for the diseases already mentioned
   soutcome <- c("heart attack" = 11, "heart failure" = 17, "pneumonia" = 23)
-
   if (any(names(soutcome)==outcome)== FALSE){stop("Invalid outcome")}
   if(any(hospitaldata$State==state)==FALSE){ stop("Invalid state")}
   #yields hospital with given state
