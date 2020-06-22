11becdb6ca05b892762e5c2c051e39f06832ec83
Abith <abithpdm@gmail.com>
2020-06-22 06:43:47 +0530

    included sorting

--- rankhospital.R
+++ rankhospital.R
@@ -4,5 +4,8 @@ rankhospital <- function(state ,outcome ,num="best")
   soutcome <- c("heart attack" = 11, "heart failure" = 17, "pneumonia" = 23)
   if (any(names(soutcome)==outcome)==FALSE){stop("Invalid Outcome")}
   if(any(hospitaldata$State==state)==FALSE){stop("Invalid State")}
-  
+  states <-subset(hospitaldata,hospitaldata$State==state)
+ sortedlist <- order(states[,soutcome[outcome]])
+ 
+ 
 }
