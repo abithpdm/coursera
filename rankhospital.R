134e85a0446852e3621a63725a3dc75d789403fa
Abith <abithpdm@gmail.com>
2020-06-22 06:28:41 +0530

    recommit

--- rankhospital.R
+++ rankhospital.R
@@ -4,4 +4,5 @@ rankhospital <- function(state ,outcome ,num="best")
   soutcome <- c("heart attack" = 11, "heart failure" = 17, "pneumonia" = 23)
   if (any(names(soutcome)==outcome)==FALSE){stop("Invalid Outcome")}
   if(any(hospitaldata$State==state)==FALSE){stop("Invalid State")}
+  
 }
