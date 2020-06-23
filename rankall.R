8ead4d0495c30aad95cfa7b5c070db347768af46
Abith <abithpdm@gmail.com>
2020-06-23 08:28:27 +0530

    Complete Code

--- rankall.R
+++ rankall.R
@@ -1,5 +1,5 @@
 #R script to rank all thospitals based on outcome in every state
-rankall<-function(outcome,num="best")
+rankall<-function(outcome,ranknum="best")
 {
   hospitaldata <-read.csv("outcome-of-care-measures.csv",colClasses = "character")
   #create a table of all states in the data file
@@ -10,14 +10,25 @@ rankall<-function(outcome,num="best")
                         Col = c(11, 17, 23))
   if(nrow(inputdf[inputdf$Outcome==outcome,])==0)
     stop("Invalid outcome")
+  hospitalname <- character(0)
   for (statename in statelist$States)
   {
     #creates a dataframe of individual states
-    statedataframe <- hospitaldata[hospitaldata$State==statename]
+    statedataframe <- hospitaldata[hospitaldata$State==statename,]
     colnumber <- inputdf[inputdf$Outcome==outcome,2]
     #remove incomplete rows
-    statedataframe <- statedataframe[complete.cases(statedataframe[,colnumber])]
+    statedataframe <- statedataframe[complete.cases(statedataframe[,colnumber]),]
+    statedataframe<-statedataframe[order(statedataframe[,colnumber],statedataframe$Hospital.Name),]
+    
+    #we declare rank number inside this for loop because rank for worst case varies according to size of df
+    if (ranknum=="best")
+      ranknum<-1
+    else if (ranknum=="worst")
+      ranknum<-nrow(statedataframe)
+    else
+      ranknum <-suppressWarnings(as.numeric(ranknum))
+    hospitalname <- c(hospitalname,statedataframe[ranknum,]$Hospital.Name)
   }
-  
+  return(data.frame(hospital = hospitalname ,state = statelist$States))
   
 }
