31d9f687d93a7e131c9aa9bc5c702ff0154cb61c
Abith <abithpdm@gmail.com>
2020-06-25 21:49:31 +0530

    csv reading documentation

--- Data_cleaning_c1.R
+++ Data_cleaning_c1.R
@@ -1,10 +1,9 @@
-file<-read.csv("datacleaning.csv")
-count <- sum(file$VAL==24,na.rm = T)
+#reading csv , use download.package() using url prior importing csv file
+datafile <- read.csv("commundata.csv")
+count<-sum(datafile$VAL==24,na.rm = T)
 
-xcelfiel <- read_excel("gasfile.xlsx")
-dat<-xcelfiel[18:23,7:15]
-url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
-xmlfile <- xmlParse(url)
-#install and load package data.frame
-DT <- fread("commundata.csv")
-
+dat <- read.xlsx("govdata.xlsx",sheetIndex = 1,colIndex = 7:15,rowIndex = 18:23)
+fileurl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+xmlfile <- xmlTreeParse("xmldata.xml",useInternalNodes= TRUE)
+rootnode <-xmlRoot(xmlfile)
+postcode <- xpathSApply(rootnode,"//zipcode",xmlValue)
